4f4200a77673ddb3f0803d543af3887a5f01646b
Gabriel Siedler <gabrielsiedler@gmail.com>
2016-06-26 20:12:54 -0300

    Adding first features

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,9 +1,39 @@
 var socket = io('http://localhost:3000');
 
 socket.on('message', function (data) {
-    $("#messages").append('<li>' + data.sender + ': ' + data.message + '</li>')
+    $(".message-box").append('<p>' + data.sender + ': ' + data.message + '</p>')
 });
 
-$('button').on('click', function () {
-    socket.emit('message', { message: $('#m').val() });
+socket.on('adminMessage', function (data) {
+    data.forEach(function (message) {
+        $(".message-box").append('<p class="admin">' + message + '</p>')
+    });
+})
+
+socket.on('receiveList', function (data) {
+    console.log('data', data);
+    data.users.forEach(function (user) {
+        $('.users-box ul').append('<li>' + user + '</li>');
+    })
 });
+
+socket.on('userConnected', function (data) {
+    $(".message-box").append('<p class="admin">* ' + data + ' joined.</p>')
+    $('.users-box ul').append('<li>' + data + '</li>');
+});
+
+socket.on('userDisconnected', function (data) {
+    $(".message-box").append('<p class="admin">* ' + data + ' left.</p>')
+    $('.users-box ul li:contains("' + data + '")').remove();
+});
+
+
+socket.on('disconnect', function () {
+    var message = '* You have ben disconnected.';
+    $(".message-box").append('<p class="system">' + message + '</p>')
+})
+
+function sendMessage () {
+    socket.emit('message', { message: $('footer input').val() });
+    $('footer input').val('');
+}
--- server/server.js
+++ server/server.js
@@ -1,23 +1,64 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const shortid = require('shortid');
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+var users = [];
 
-  socket.on('message', function (data) {
-    var message = {
-      sender: 'someone',
-      message: data.message
+var botId = shortid.generate();
+
+users[botId] = {
+    id: botId,
+    nick: '@SocketIRC_Bot'
+};
+
+function getCurrentUserList () {
+    var userNicks = [];
+    for (user in users) {
+        console.log(user);
+        userNicks.push(users[user].nick);
     }
-      io.sockets.emit('message', message);
-  });
 
-  socket.on('disconnect', function () {
-    console.log('a user disconnected');
-  });
+    return userNicks;
+}
+
+var userCount = 1;
+io.on('connection', function (socket) {
+    var player = {
+        id: shortid.generate(),
+        nick: 'Guest' + userCount++
+    }
+
+    users[player.id] = player;
+
+    socket.broadcast.emit('userConnected', player.nick);
+    console.log(player.nick + ' connected');
+
+    socket.emit('receiveList', { users: getCurrentUserList() });
+
+    var connectMessages = [
+        '* Welcome to socketIRC.',
+        '* Your nick is ' + player.nick + '. To change it type /nick [new nick]',
+        '* type /help for more instructions.'
+    ];
+
+    socket.emit('adminMessage', connectMessages);
+
+    socket.on('message', function (data) {
+        var message = {
+            sender: player.nick,
+            message: data.message
+        }
+        io.sockets.emit('message', message);
+    });
+
+    socket.on('disconnect', function () {
+        delete users[player.id];
+        socket.broadcast.emit('userDisconnected', player.nick);
+        console.log(player.nick + ' disconnected');
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
